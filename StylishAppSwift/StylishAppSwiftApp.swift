6959d3e0754527698605eab04467deb1b4bbd204
ahuamana <73962071@continental.edu.pe>
2024-02-13 19:59:13 -0500

    Initial Commit

---
+++ StylishAppSwift/StylishAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StylishAppSwiftApp.swift
+//  StylishAppSwift
+//
+//  Created by Antony Huaman Alikhan on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct StylishAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
