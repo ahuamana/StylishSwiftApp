2a6ac1d5f533cc96ef50c8373fe73d27e428a77b
ahuamana <73962071@continental.edu.pe>
2024-04-24 22:26:38 -0500

    added navigation views

---
+++ StylishAppSwift/Screens/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  StylishAppSwift
+//
+//  Created by Antony Huaman Alikhan on 24/04/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
